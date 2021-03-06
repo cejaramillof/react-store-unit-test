068963aecfdd06ff1e2894b9a23737de7e2f1144
Carlos Jaramillo <Carlos.Jaramillo@blackboard.com>
2021-03-06 09:43:18 -0500

    configure store, create reducers, actions

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
 
+//import { createStore } from 'redux';
 // import App from './store/routes/App';
-import initialState from './store/initialState';
-import reducer from './store/reducers';
+// import initialState from './store/initialState';
+// import reducer from './store/reducers';
+
+// const store = createStore(reducer, initialState);
 
 // import App from './counter/App';
-import App from './jotto/context/App';
+import App from './jotto/App';
 
 import './index.css';
-
-const store = createStore(reducer, initialState);
 
 ReactDOM.render(
   <Provider store={store}>
