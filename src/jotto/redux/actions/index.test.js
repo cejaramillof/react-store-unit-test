ae21c25c3f4b2f43ce8dc6cc2825f3899391c1c5
Carlos Jaramillo <Carlos.Jaramillo@blackboard.com>
2021-03-21 11:33:42 -0500

    add secretWord action creator and reducer

--- src/jotto/redux/actions/index.test.js
+++ src/jotto/redux/actions/index.test.js
@@ -1,9 +1,36 @@
-import { correctGuess, actionTypes } from '.';
+import moxios from 'moxios';
+import { correctGuess, actionTypes, getSecretWord } from '.';
+import { storeFactory } from '../../../testUtils';
 
 describe('correctGuess', () => {
   test('returns an action with type `CORRECT_GUESS`', () => {
     const action = correctGuess();
     // expect(action).toBe({}); // for the immutability
     expect(action).toEqual({ type: actionTypes.CORRECT_GUESS });
+  });
+});
+
+describe('getSecretWord action creator', () => {
+  beforeEach(() => {
+    moxios.install(); // receive instance of axios as a param
+  });
+  afterEach(() => {
+    moxios.uninstall();
+  });
+  test('add response word to state', () => {
+    const secretWord = 'party';
+    const store = storeFactory();
+    moxios.wait(() => {
+      const request = moxios.requests.mostRecent();
+      request.respondWith({
+        status: 200,
+        response: secretWord,
+      });
+    });
+    return store.dispatch(getSecretWord())
+      .then(() => {
+        const newState = store.getState();
+        expect(newState.secretWord).toBe(secretWord);
+      });
   });
 });
